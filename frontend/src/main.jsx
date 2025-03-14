add8ad94b6d4ea683804f8b342d92565520f463f
Stanley-enumahh <enumah44@gmail.com>
2025-03-14 04:35:23 +0300

    solved vercel error

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,12 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import SignUp from "./pages/SignUp.jsx";
-import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
 import { AuthProvider } from "./context.jsx/AuthContext.jsx";
 import LoginPage from "./pages/Login.jsx";
 import { ResetPassword } from "./pages/resetPassword.jsx";
-// import PrivateRoute from "./components/PrivateRoute.jsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient({
