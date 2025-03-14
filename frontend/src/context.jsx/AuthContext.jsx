72b69da2d3f3565c580c5da23740d225b0e41e73
Stanley-enumahh <enumah44@gmail.com>
2025-03-14 04:57:16 +0300

    solved vercel error

--- frontend/src/context.jsx/AuthContext.jsx
+++ frontend/src/context.jsx/AuthContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext } from "react";
 import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 import { login, signup, logout, getProfile } from "../components/Auth";
 import { useNavigate } from "react-router";
