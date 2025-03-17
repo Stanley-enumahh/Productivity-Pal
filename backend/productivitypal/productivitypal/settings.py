b319ffece90b9d6a7f6f954ac64a6a76718683cf
Victor <victorokolie001@gmail.com>
2025-03-17 18:27:24 +0100

    fixed typo error in setting.py

--- backend/productivitypal/productivitypal/settings.py
+++ backend/productivitypal/productivitypal/settings.py
@@ -180,7 +180,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Emailing
