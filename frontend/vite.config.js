46dbc93958796f07d36a34c01d74609b7caa0299
Stanley-enumahh <enumah44@gmail.com>
2025-03-25 10:57:44 +0300

    note creation

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     port: 3000,
   },
-  base: "/", // Ensures correct base path
+  base: "./", // Ensures correct base path
   build: {
     outDir: "dist", // Vercel expects the output in "dist"
   },
