b688e46080b691e5b030aa09dc063cff1452e7ed
Victor <victorokolie001@gmail.com>
2025-04-15 14:19:04 +0100

    Edited todo endpoint and serializer

--- backend/productivitypal/api/tests.py
+++ backend/productivitypal/api/tests.py
@@ -4,19 +4,19 @@ import requests
 # Create your tests here.
 
 
-url =  'http://127.0.0.1:8000/api/todoitems/?todo=16'
+url =  'http://127.0.0.1:8000/api/todos/'
 
 data = {
-    'todo': 16,
-    "title": "Buy coconut",
-    'completed': False
+    'title': 'first todo',
+    "todoitems": [{"title": 'first todo'}, {'title': 'second todo'}],
+    'due_date': "2025-04-15T13:45:30.000Z"
 }
 
 
 
 
 refresh_token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoicmVmcmVzaCIsImV4cCI6MTc0MjQ4NjQxMywiaWF0IjoxNzQyNDAwMDEzLCJqdGkiOiIzZmJlNzkzYjAzYTk0YjZiOGNjYzE1ZWFjYmZlMjFiOSIsInVzZXJfaWQiOjF9.PkOydeo6H0rP7S6__V6ktcQygXPV3ugWYr0JVhLlyoc'
-access_token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzQyNDIzMDA5LCJpYXQiOjE3NDI0MTU4MDksImp0aSI6IjJkYTczY2FkZDllYTQyZDI4OThlZTA4MjBiNzI0ZDI2IiwidXNlcl9pZCI6MX0.3-puPPe-AHnXy_thCpJu-Kbsc_V3ixwp438OEHWrRoM'
+access_token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzQ0NzI0MTY5LCJpYXQiOjE3NDQ3MTY5NjksImp0aSI6IjE1NWQyYzRkZmMzMTRkYjdhYmE4MGNmYjhiYWY4NDNkIiwidXNlcl9pZCI6MX0._BNcK5yjdrmuZiybYvfmWQw-dOoeYTbYYYMKKyDTb7g'
 
 headers = {
     'Authorization': f"Bearer {access_token}"
